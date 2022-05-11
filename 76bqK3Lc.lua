9566c1dba5b686a36236e08230ac414f5d737be8
Ausommet <48718483+Ausommet@users.noreply.github.com>
2022-05-11 23:18:48 +0100

    Update and rename autopickup.lua to 76bqK3Lc.lua

--- 76bqK3Lc.lua
+++ 76bqK3Lc.lua
@@ -5,11 +5,11 @@ local Character = Player.Character
 local Hitbox = Character:WaitForChild("hitbox") 
 local PlayerRequest = ReplicatedStorage:WaitForChild("playerRequest")
 
-local function PickupItem(Item) 
+local function bqK3Lc76(Item) 
     PlayerRequest:InvokeServer("pickUpItemRequest", Item)
 end
 
-local function AutoPickup()
+local function bqK3Lc()
     if not Hitbox then
         return 
     end
@@ -17,11 +17,11 @@ local function AutoPickup()
     for Index, Value in next, workspace.placeFolders.items:GetChildren() do 
         RunService.Heartbeat:Wait()
         if (Value.Position - Hitbox.Position).Magnitude < 10 then 
-            PickupItem(Value)
+            bqK3Lc76(Value)
         end
     end
 end
 
 while RunService.Heartbeat:Wait() do
-        coroutine.wrap(AutoPickup)()
+        coroutine.wrap(bqK3Lc)()
 end
