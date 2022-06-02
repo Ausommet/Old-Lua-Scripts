b2522f2a2e2a115c2889808bc25ce3388bf9b384
Admin <0nth3cl0uds@protonmail.com>
2022-06-02 09:11:36 +0100

    Auto-rejoin / Staff Detection

--- auto-farm.lua
+++ auto-farm.lua
@@ -45,11 +45,89 @@ local Main = UI.New({
     Title = "Main"
 })
 
+--Builders
 local Players = game:GetService("Players")
 local Client = Players.LocalPlayer.Name
 local RunService = game:GetService("RunService")
 local Imput = game:GetService("UserInputService")
 local Plr = game.Players.LocalPlayer
+local bodyvelocityenabled = true
+local Chr = Plr.Character
+local ts = game:GetService("TweenService")
+local lc = game.Players.LocalPlayer.Character
+local hm = lc.HumanoidRootPart
+local Player = Players.LocalPlayer
+local PlaceID = game.PlaceId
+local AllIDs = {}
+local foundAnything = ""
+local actualHour = os.date("!*t").hour
+local Deleted = false
+local Mobs = {}
+
+--Server Hop Section
+local File = pcall(function()
+    AllIDs = game:GetService('HttpService'):JSONDecode(readfile("NotSameServers.json"))
+end)
+if not File then
+    table.insert(AllIDs, actualHour)
+    writefile("NotSameServers.json", game:GetService('HttpService'):JSONEncode(AllIDs))
+end
+function TPReturner()
+    local Site;
+    if foundAnything == "" then
+        Site = game.HttpService:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. PlaceID .. '/servers/Public?sortOrder=Asc&limit=100'))
+    else
+        Site = game.HttpService:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. PlaceID .. '/servers/Public?sortOrder=Asc&limit=100&cursor=' .. foundAnything))
+    end
+    local ID = ""
+    if Site.nextPageCursor and Site.nextPageCursor ~= "null" and Site.nextPageCursor ~= nil then
+        foundAnything = Site.nextPageCursor
+    end
+    local num = 0;
+    for i,v in pairs(Site.data) do
+        local Possible = true
+        ID = tostring(v.id)
+        if tonumber(v.maxPlayers) > tonumber(v.playing) then
+            for _,Existing in pairs(AllIDs) do
+                if num ~= 0 then
+                    if ID == tostring(Existing) then
+                        Possible = false
+                    end
+                else
+                    if tonumber(actualHour) ~= tonumber(Existing) then
+                        local delFile = pcall(function()
+                            delfile("NotSameServers.json")
+                            AllIDs = {}
+                            table.insert(AllIDs, actualHour)
+                        end)
+                    end
+                end
+                num = num + 1
+            end
+            if Possible == true then
+                table.insert(AllIDs, ID)
+                wait()
+                pcall(function()
+                    writefile("NotSameServers.json", game:GetService('HttpService'):JSONEncode(AllIDs))
+                    wait()
+                    game:GetService("TeleportService"):TeleportToPlaceInstance(PlaceID, ID, game.Players.LocalPlayer)
+                end)
+                wait(4)
+            end
+        end
+    end
+end
+
+function Teleport()
+    while wait() do
+        pcall(function()
+            TPReturner()
+            if foundAnything ~= "" then
+                TPReturner()
+            end
+        end)
+    end
+end
 
 --Kill Aura Section
 Main.Toggle({
@@ -64,7 +142,7 @@ Main.Toggle({
         for Index, Value in next, workspace.Mobs:GetChildren() do
             RunService.Heartbeat:Wait(atkspeed / 2)
             if workspace.Mobs:FindFirstChild(Value.Name) and workspace.Mobs[Value.Name]:FindFirstChild('Head') then
-              if (Value['Head'].Position - workspace[Client]['Head'].Position).magnitude < distance  then 
+              if (Value['Head'].Position - workspace[Client]['Head'].Position).magnitude < (distance * 10)  then 
                 game:GetService("ReplicatedStorage").ChangeWeld:FireServer("One-Handed Held", "RightLowerArm")
                 game:GetService("ReplicatedStorage").DamageMob:FireServer(workspace.Mobs[Value.Name].Humanoid, false, workspace[Client].Sword.Middle)
               RunService.Heartbeat:Wait(atkspeed / 2)
@@ -81,13 +159,13 @@ Main.Toggle({
     Callback = function(Value)
             distance = Value
         end,
-    Min = 100,
-    Max = 250,
-    Def = 100,
+    Min = 10,
+    Max = 25,
+    Def = 10,
 })
 
 if distance == nil then
-    distance = 100
+    distance = 10
 end
 
 Main.Slider({
@@ -104,14 +182,6 @@ if atkspeed == nil then
 end
 
 --Auto Farm Section
-local bodyvelocityenabled = true
-local Chr = Plr.Character
-local ts = game:GetService("TweenService")
-local char = game.Players.LocalPlayer.Character
-local hm = char.HumanoidRootPart
-local lc = game.Players.LocalPlayer.Character
-local hm = lc.HumanoidRootPart
-local Mobs = {}
 
 local function Mob_Update()
     for i, v in pairs(workspace.Mobs:GetChildren()) do
@@ -199,3 +269,39 @@ local Select = Main.Dropdown({
             Select:SetOptions(Mobs)
         end,
     })
+    
+-- Staff Detection // Re-join On kick
+    Players.PlayerAdded:Connect(function(Plr)
+        if Settings.StaffDetection and Plr:GetRankInGroup(5683480) > 1 then 
+                        Teleport()
+                    end
+                end)
+
+    Main.Toggle({
+        Text= 'Staff Detection',
+        Callback = function(Value)
+            if Value then 
+                for Index, Value in next, Players:GetPlayers() do 
+                    if Value ~= Player and Value:GetRankInGroup(5683480) > 1 then 
+                        while wait() do 
+                            Teleport()
+                                end
+                            end
+                        end
+                    end
+            end,
+            staffdetection = false
+        })
+        Main.Toggle({
+            Text= 'Auto Rejoin',
+            Callback = function(Value)
+                if Value then 
+                    getgenv().rejoin = game:GetService("CoreGui").RobloxPromptGui.promptOverlay.ChildAdded:Connect(function(child)
+                        if child.Name == 'ErrorPrompt' and child:FindFirstChild('MessageArea') and child.MessageArea:FindFirstChild("ErrorFrame") then
+                            game:GetService("TeleportService"):Teleport(game.PlaceId)
+                        end
+                    end)
+                end
+                end,
+                autorejoin = false
+            })
