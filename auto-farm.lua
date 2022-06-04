529e63b5f4bbc7b7b8be66a0d968ec6d64fbb623
Admin <0nth3cl0uds@protonmail.com>
2022-06-04 20:14:49 +0100

    changed speed :/ --cheeky devs hello

--- auto-farm.lua
+++ auto-farm.lua
@@ -179,7 +179,7 @@ local function to(newpos)
     if Chr ~= nil then
         local ts = game:GetService("TweenService")
         local dist = (hm.Position - target.HumanoidRootPart.Position).magnitude
-        local tweenspeed = dist / 40
+        local tweenspeed = dist / 30
         local ti = TweenInfo.new(tonumber(tweenspeed), Enum.EasingStyle.Linear)
         local tp = {CFrame = CFrame.new(newpos)}
         local tween =  ts:Create(hm, ti, tp)
