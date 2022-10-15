a4affac11b09aed52fe4043d05bc8d297854581b
Ausommet <0nth3cl0uds@protonmail.com>
2022-10-15 05:26:20 +0100

    Renamed

--- Vesteria/Vesteria-Gui.lua
+++ Vesteria/Vesteria-Gui.lua
@@ -1,7 +1,7 @@
 local Material = loadstring(game:HttpGet("https://raw.githubusercontent.com/Kinlei/MaterialLua/master/Module.lua"))()
 
 local UI = Material.Load({
-    Title = "ZZZZ Vesteria | by Z44_X",
+    Title = "esteria",
     Style = 3,
     SizeX = 300,
     SizeY = 115,
